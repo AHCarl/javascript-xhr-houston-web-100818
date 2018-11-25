5c5dfaa2ec921a13dfd7cbe66b19dda3c00e0343
Alexander Carlin <ahcarlin@gmail.com>
2018-11-25 23:30:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 function showRepositories(event, data) {
   let repos = JSON.parse(this.responseText);
   console.log(repos);
-  
+  const repoList = `<ul>${repos
+    .map(r => "<li>" + r.name + "</li>")
+    .join("")}</ul>`;
   document.getElementById("repositories").innerHTML = repoList;
 }
 
