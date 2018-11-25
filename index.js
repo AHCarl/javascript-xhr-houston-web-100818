b26a199e42714fd805742ca0225cf7fe3c0b5a20
Alexander Carlin <ahcarlin@gmail.com>
2018-11-25 23:35:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ function showRepositories(event, data) {
   document.getElementById("repositories").innerHTML = repoList;
 }
 
-
+function showCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        "<li><strong>" +
+        commit.commit.author.name +
+        "</strong> - " +
+        commit.commit.message +
+        "</li>"
+    )
+    .join("")}</ul>`;
+  document.getElementById("commits").innerHTML = commitsList;
+}
 
 function getRepositories() {
   const req = new XMLHttpRequest();
